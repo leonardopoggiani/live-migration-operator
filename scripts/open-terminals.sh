0bdb3f76c079a4e787078ee81f2c06ccd277ac81
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-02-27 16:49:00 +0100

    Update Dockerfile, scripts and workflow files for build and publication

--- scripts/open-terminals.sh
+++ scripts/open-terminals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 xfce4-terminal -T "ubuntu@172.16.3.227" -x bash -c "ssh -t ubuntu@172.16.3.227; bash" &
-xfce4-terminal -T "ubuntu@172.16.6.52" -x bash -c "ssh -t ubuntu@172.16.6.52; bash" &
+xfce4-terminal -T "ubuntu@172.16.3.227" -x bash -c "ssh -t ubuntu@172.16.3.227; bash" &
 xfce4-terminal -T "ubuntu@172.16.3.79" -x bash -c "ssh -t ubuntu@172.16.3.79; bash" &
-xfce4-terminal -T "ubuntu@172.16.6.63" -x bash -c "ssh -t ubuntu@172.16.6.63; bash" &
+xfce4-terminal -T "ubuntu@172.16.3.79" -x bash -c "ssh -t ubuntu@172.16.3.79; bash" &
 
