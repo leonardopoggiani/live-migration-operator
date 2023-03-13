9aad8d27f96006e8a0e61500fcf99ddc2da3a7f5
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-03-13 19:42:15 +0100

    Update klog and command formatting for improved readability

--- controllers/livemigration_controller.go
+++ controllers/livemigration_controller.go
@@ -933,8 +933,8 @@ func tryBuildah(ctx context.Context, container Container) error {
 
 	// err = builder.Add(builder.ContainerID, false, buildah.AddAndCopyOptions{}, "checkpoint/"+container.ID+".tar")
 
-	klog.Infof("\nsudo buildah add " + newContainer + " /home/ubuntu/live-migration-operator/checkpoint/" + container.ID + ".tar /")
-	addCheckpointCmd := exec.Command("/bin/sh", "-c", "sudo buildah add "+newContainer+" /home/ubuntu/live-migration-operator/checkpoint/"+container.ID+".tar /")
+	klog.Infof("\nsudo buildah add ", newContainer, " /home/ubuntu/live-migration-operator/checkpoint/"+container.ID+".tar /")
+	addCheckpointCmd := exec.Command("/bin/sh", "-c", "sudo buildah add ", newContainer, "/home/ubuntu/live-migration-operator/checkpoint/"+container.ID+".tar", "/")
 	// addCheckpointCmd := exec.Command("sudo", "buildah", "add", newContainer, "/home/ubuntu/live-migration-operator/checkpoint/"+container.ID+".tar")
 	out, err := addCheckpointCmd.CombinedOutput()
 	if err != nil {
