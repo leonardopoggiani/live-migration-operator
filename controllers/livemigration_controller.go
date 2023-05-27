aa02671bba5e7284b51fcb56fd7e002db28b7c28
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-05-27 23:59:50 +0200

    fix

--- controllers/livemigration_controller.go
+++ controllers/livemigration_controller.go
@@ -43,6 +43,7 @@ type LiveMigrationReconciler struct {
 
 func (r *LiveMigrationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	klog.Infof("Reconciling LiveMigration %s", req.Name)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// Load Kubernetes config
 	kubeconfigPath := os.Getenv("KUBECONFIG")
@@ -536,24 +537,6 @@ func (r *LiveMigrationReconciler) MigrateCheckpoint(ctx context.Context, directo
 		} else {
 			klog.Infof("post on the service", "service", "dummy-service", "out", string(postOut))
 		}
-	}
-
-	// send a dummy file at the end to signal the end of the migration
-	createDummyFile := exec.Command("sudo", "touch", "/tmp/checkpoints/checkpoints/dummy")
-	createDummyFileOutput, err := createDummyFile.Output()
-	if err != nil {
-		klog.ErrorS(err, "failed to create dummy file", "output", createDummyFileOutput)
-	}
-
-	dummyPath := "/tmp/checkpoints/checkpoints/dummy"
-
-	postCmd := exec.Command("curl", "-X", "POST", "-F", fmt.Sprintf("file=@%s", dummyPath), fmt.Sprintf("http://%s:%d/upload", dummyIp, dummyPort))
-	klog.Infof("post command", "cmd", postCmd.String())
-	postOut, err := postCmd.CombinedOutput()
-	if err != nil {
-		klog.ErrorS(err, "failed to post on the service", "service", "dummy-service")
-	} else {
-		klog.Infof("post on the service", "service", "dummy-service", "out", string(postOut))
 	}
 
 	return nil
