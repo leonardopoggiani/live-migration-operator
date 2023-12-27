6f90de69b4a1de9cd2e0ddbd1c9da98bef7a4df0
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-12-27 09:50:03 +0100

    feat(operator): update gitignore and checkpoint methods

--- controllers/crio.go
+++ controllers/crio.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/leonardopoggiani/live-migration-operator/controllers/types"
@@ -10,17 +11,17 @@ import (
 func (r *LiveMigrationReconciler) CheckpointPodCrio(containers []types.Container, namespace string, podName string) error {
 	// curl -sk -X POST "https://localhost:10250/checkpoint/liqo-demo/tomcat-pod/tomcat"
 
+	curlPath := "https://localhost:10250/checkpoint/%s/%s/%s"
+
 	for _, container := range containers {
 		klog.Info("[INFO]", "checkpointing container", "container", container.Name)
 
-		curlPath := "https://localhost:10250/checkpoint/" + namespace + "/" + podName + "/" + container.Name
-		checkpointCmd := exec.Command("curl", "-sk", "-XPOST", curlPath)
-
+		checkpointCmd := exec.Command("curl", "-sk", "-XPOST", fmt.Sprintf(curlPath, namespace, podName, container.Name))
 		output, err := checkpointCmd.CombinedOutput()
+
 		if err != nil {
 			klog.ErrorS(err, "failed to checkpoint container", "output", string(output))
 		}
-
 	}
 
 	return nil
